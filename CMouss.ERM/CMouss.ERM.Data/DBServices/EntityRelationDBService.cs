54d1c1386d50f0cccad8f2b5432a32b92ef150bd
Caesar Moussalli <cmoussalli@gmail.com>
2025-05-05 14:46:46 +0400

    DBService & ServingService: EntityRelation

--- CMouss.ERM/CMouss.ERM.Data/DBServices/EntityRelationDBService.cs
+++ CMouss.ERM/CMouss.ERM.Data/DBServices/EntityRelationDBService.cs
@@ -61,7 +61,50 @@ namespace CMouss.ERM.Data.DBServices
             return entityRelations;
         }
 
-      
+        public async Task<EntityRelation> AddAsync( string relationName, int entityTypeId_Left, int entityTypeId_Right, string title_Left, string title_Right, bool isList_Left, bool isList_Right, bool isRequired_Left, bool isRequired_Right)
+        {
+            var entityRelation = new EntityRelation
+            {
+                Name = relationName,
+
+                EntityTypeId_Left = entityTypeId_Left,
+                EntityTypeId_Right = entityTypeId_Right,
+
+                Title_Left = title_Left,
+                Title_Right = title_Right,
+                IsRequired_Left = isRequired_Left,
+                IsRequired_Right = isRequired_Right,
+                IsList_Left = isList_Left,
+                IsList_Right = isList_Right,
+                
+                IsDeleted = false,
+
+            };
+            await _context.EntityRelations.AddAsync(entityRelation);
+            await _context.SaveChangesAsync();
+            return entityRelation;
+        }
+
+        public async Task<EntityRelation> UpdateAsync(int id, string relationName, int entityTypeId_Left, int entityTypeId_Right, string title_Left, string title_Right, bool isList_Left, bool isList_Right, bool isRequired_Left, bool isRequired_Right)
+        {
+            var entityRelation = await _context.EntityRelations
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (entityRelation == null)
+            {
+                throw new Exception("Entity Relation not found");
+            }
+            entityRelation.Name = relationName;
+            entityRelation.EntityTypeId_Left = entityTypeId_Left;
+            entityRelation.EntityTypeId_Right = entityTypeId_Right;
+            entityRelation.Title_Left = title_Left;
+            entityRelation.Title_Right = title_Right;
+            entityRelation.IsRequired_Left = isRequired_Left;
+            entityRelation.IsRequired_Right = isRequired_Right;
+            entityRelation.IsList_Left = isList_Left;
+            entityRelation.IsList_Right = isList_Right;
+            await _context.SaveChangesAsync();
+            return entityRelation;
+        }
 
         public async Task<bool> DeleteAsync(int id)
         {
@@ -72,6 +115,23 @@ namespace CMouss.ERM.Data.DBServices
                 throw new Exception("Entity Relation not found");
             }
             entityRelation.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteByEntityTypeId(int entityTypeId)
+        {
+            var entityRelations = await _context.EntityRelations
+                .Where(x => x.EntityTypeId_Left == entityTypeId || x.EntityTypeId_Right == entityTypeId)
+                .ToListAsync();
+            if (entityRelations.Count == 0)
+            {
+                throw new Exception("Entity Relation not found");
+            }
+            foreach (var entityRelation in entityRelations)
+            {
+                entityRelation.IsDeleted = true;
+            }
             await _context.SaveChangesAsync();
             return true;
         }
---
+++ CMouss.ERM/CMouss.ERM.Serving/ServingServices/EntityRelationService.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMouss.ERM.Data;
+
+namespace CMouss.ERM.Serving.ServingServices
+{
+    public class EntityRelationService
+    {
+
+        ServingServiceParams _servingServiceParams;
+
+        ERMDBContext db = new();
+
+        DBService dbService = new();
+
+        public EntityRelationService(ServingServiceParams servingServiceParams)
+        {
+            _servingServiceParams = servingServiceParams;
+        }
+
+        public async Task<List<EntityRelation>> GetAllAsync()
+        {
+            List<EntityRelation> result = new();
+            List<Data.DBModels.EntityRelation> ers = await dbService.EntityRelationDBService.GetAllAsync();
+            if (ers.Count > 0)
+            {
+                result.AddRange(new Adapters.EntityRelationAdapter().Convert(ers));
+            }
+            return result;
+        }
+
+        public async Task<EntityRelation> getByIdAsync(int id)
+        {
+            EntityRelation result = new();
+            Data.DBModels.EntityRelation er = await dbService.EntityRelationDBService.GetByIdAsync(id);
+            if (er is not null)
+            {
+                result = new Adapters.EntityRelationAdapter().Convert(er);
+            }
+            return result;
+        }
+
+        public async Task<List<EntityRelation>> GetByEntityTypeId_LeftAsync(int entityTypeId)
+        {
+            List<EntityRelation> result = new();
+            List<Data.DBModels.EntityRelation> ers = await dbService.EntityRelationDBService.GetByEntityTypeId_LeftAsync(entityTypeId);
+            if (ers.Count > 0)
+            {
+                result.AddRange(new Adapters.EntityRelationAdapter().Convert(ers));
+            }
+            return result;
+        }
+
+        public async Task<List<EntityRelation>> GetByEntityTypeId_RightAsync(int entityTypeId)
+        {
+            List<EntityRelation> result = new();
+            List<Data.DBModels.EntityRelation> ers = await dbService.EntityRelationDBService.GetByEntityTypeId_RightAsync(entityTypeId);
+            if (ers.Count > 0)
+            {
+                result.AddRange(new Adapters.EntityRelationAdapter().Convert(ers));
+            }
+            return result;
+        }
+
+        public async Task<EntityRelation> AddAsync(string relationName, int entityTypeId_Left, int entityTypeId_Right, string title_Left, string title_Right, bool isList_Left, bool isList_Right, bool isRequired_Left, bool isRequired_Right)
+        {
+            EntityRelation result = new();
+            Data.DBModels.EntityRelation er = await dbService.EntityRelationDBService.AddAsync(relationName, entityTypeId_Left, entityTypeId_Right, title_Left, title_Right, isList_Left, isList_Right, isRequired_Left, isRequired_Right);
+            if (er is not null)
+            {
+                result = new Adapters.EntityRelationAdapter().Convert(er);
+            }
+            return result;
+        }
+
+        public async Task<EntityRelation> UpdateAsync(int id, string relationName, int entityTypeId_Left, int entityTypeId_Right, string title_Left, string title_Right, bool isList_Left, bool isList_Right, bool isRequired_Left, bool isRequired_Right)
+        {
+            EntityRelation result = new();
+            Data.DBModels.EntityRelation er = await dbService.EntityRelationDBService.UpdateAsync(id, relationName, entityTypeId_Left, entityTypeId_Right, title_Left, title_Right, isList_Left, isList_Right, isRequired_Left, isRequired_Right);
+            if (er is not null)
+            {
+                result = new Adapters.EntityRelationAdapter().Convert(er);
+            }
+            return result;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            bool result = false;
+            result = await dbService.EntityRelationDBService.DeleteAsync(id);
+            return result;
+        }
+
+        public async Task<bool> DeleteByEntityTypeId(int entityTypeId)
+        {
+            bool result = false;
+            result = await dbService.EntityRelationDBService.DeleteByEntityTypeId(entityTypeId);
+            return result;
+        }
+
+
+        public async Task<bool> RestoreAsync(int id)
+        {
+            bool result = false;
+            result = await dbService.EntityRelationDBService.RestoreAsync(id);
+            return result;
+        }
+
+        public async Task<List<EntityRelation>> GetAllDeletedAsync()
+        {
+            List<EntityRelation> result = new();
+            List<Data.DBModels.EntityRelation> ers = await dbService.EntityRelationDBService.GetAllDeletedAsync();
+            if (ers.Count > 0)
+            {
+                result.AddRange(new Adapters.EntityRelationAdapter().Convert(ers));
+            }
+            return result;
+        }
+
+
+
+    }
+
+
+
+}
