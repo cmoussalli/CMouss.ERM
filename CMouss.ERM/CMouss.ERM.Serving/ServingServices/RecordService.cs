54e2b50f3942d20d4ec8070460e4ed9b3423f942
Caesar Moussalli <cmoussalli@gmail.com>
2025-05-06 11:27:00 +0400

    Record Service: GetByEntityTypeIdAsync

---
+++ CMouss.ERM/CMouss.ERM.Serving/ServingServices/RecordService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMouss.ERM.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMouss.ERM.Serving.ServingServices
+{
+    public class RecordService
+    {
+
+        ServingServiceParams _servingServiceParams;
+
+        ERMDBContext db = new();
+
+        DBService dbService = new();
+
+        public RecordService(ServingServiceParams servingServiceParams)
+        {
+            _servingServiceParams = servingServiceParams;
+        }
+
+
+        public async Task<List<Record>> GetByEntityTypeIdAsync(int entityTypeId)
+        {
+            List<Record> result = new();
+            var records = await db.Records
+                .Where(r => r.EntityTypeId == entityTypeId)
+                .ToListAsync();
+
+            foreach (var r in records)
+            {
+                result.Add(new Adapters.RecordAdapter().Convert(r));
+            }
+
+            return result;
+        }
+
+
+    }
+}
